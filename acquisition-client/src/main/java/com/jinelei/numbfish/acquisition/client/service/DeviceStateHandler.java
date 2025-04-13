39fd0f7e33003447ca1931a59a4a9d029e06f0e5
jinelei <jinelei@gmail.com>
2025-04-13 21:02:48 +0800

    优化更新设备运行

--- acquisition-client/src/main/java/com/jinelei/numbfish/acquisition/client/service/DeviceStateHandler.java
+++ acquisition-client/src/main/java/com/jinelei/numbfish/acquisition/client/service/DeviceStateHandler.java
@@ -3,6 +3,9 @@ package com.jinelei.numbfish.acquisition.client.service;
 import com.jinelei.numbfish.acquisition.client.influx.InfluxService;
 import com.jinelei.numbfish.acquisition.client.influx.bean.DeviceStateMessage;
 import com.jinelei.numbfish.common.exception.InternalException;
+import com.jinelei.numbfish.common.view.BaseView;
+import com.jinelei.numbfish.device.api.DeviceApi;
+import com.jinelei.numbfish.device.dto.DeviceRunningStateUpdateRequest;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +15,8 @@ import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Optional;
 
 /**
@@ -25,9 +30,11 @@ public class DeviceStateHandler implements MessageHandler, InitializingBean {
     private static final Logger log = LoggerFactory.getLogger(DeviceStateHandler.class);
 
     private final InfluxService influxService;
+    private final DeviceApi deviceApi;
 
-    public DeviceStateHandler(InfluxService influxService) {
+    public DeviceStateHandler(InfluxService influxService, DeviceApi deviceApi) {
         this.influxService = influxService;
+        this.deviceApi = deviceApi;
     }
 
     @Override
@@ -39,6 +46,20 @@ public class DeviceStateHandler implements MessageHandler, InitializingBean {
             }
         } catch (Throwable throwable) {
             log.error("saveDeviceRunState failure: {}", throwable.getMessage());
+        }
+        try {
+            if (message.getPayload() instanceof DeviceStateMessage ds) {
+                Optional.ofNullable(deviceApi).ifPresent(api -> {
+                    DeviceRunningStateUpdateRequest request = new DeviceRunningStateUpdateRequest();
+                    request.setDeviceId(ds.getDeviceCode());
+                    request.setState(ds.getState());
+                    request.setTimestamp(LocalDateTime.ofInstant(ds.getTime(), ZoneId.systemDefault()));
+                    BaseView<Void> result = api.updateRunningState(request);
+                    log.debug("updateRunningState success: {}", result);
+                });
+            }
+        } catch (Throwable throwable) {
+            log.error("updateRunningState failure: {}", throwable.getMessage());
         }
     }
 
