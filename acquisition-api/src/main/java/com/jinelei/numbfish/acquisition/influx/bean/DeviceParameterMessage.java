157a4e956255cd1de6fec140934ab1107019da2c
jinelei <jinelei@163.com>
2025-04-18 23:15:34 +0800

    添加InfluxService及相关消息类，支持设备状态、参数和生产数据的查询与保存

---
+++ acquisition-api/src/main/java/com/jinelei/numbfish/acquisition/influx/bean/DeviceParameterMessage.java
@@ -0,0 +1,103 @@
+package com.jinelei.numbfish.acquisition.influx.bean;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * @Author: jinelei
+ * @Description:
+ * @Date: 2024/7/14
+ * @Version: 1.0.0
+ */
+@SuppressWarnings("unused")
+public class DeviceParameterMessage extends AbstractMessage {
+    public static final String NAME = "name";
+    public static final String VALUE1 = "value";
+    private String name;
+    private Object value;
+
+    @Override
+    public String bucket() {
+        return "DeviceParameter";
+    }
+
+    @Override
+    public String measurement() {
+        return "%s_%s".formatted(getDeviceCode(), getName());
+    }
+
+    @Override
+    public Map<String, String> tags() {
+        return Map.of(DEVICE_CODE, getDeviceCode(), NAME, getName());
+    }
+
+    @Override
+    public Map<String, Object> fields() {
+        return Map.of(VALUE1, getValue());
+    }
+
+    @SafeVarargs
+    public final DeviceParameterMessage parse(final Map<String, Object>... maps) {
+        for (Map<String, Object> map : maps) {
+            Optional.ofNullable(map.get(DEVICE_CODE)).map(Object::toString).ifPresent(this::setDeviceCode);
+            Optional.ofNullable(map.get(TIME)).map(Object::toString).map(Instant::parse).ifPresent(this::setTime);
+            Optional.ofNullable(map.get(NAME)).map(Object::toString).ifPresent(this::setName);
+            Optional.ofNullable(map.get(VALUE)).ifPresent(this::setValue);
+        }
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public DeviceParameterMessage() {
+    }
+
+    public DeviceParameterMessage(String deviceCode, Instant time, String name, Object value) {
+        super.setDeviceCode(deviceCode);
+        super.setTime(time);
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof DeviceParameterMessage that))
+            return false;
+        if (!super.equals(o))
+            return false;
+        return Objects.equals(name, that.name) && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name, value);
+    }
+
+    @Override
+    public String toString() {
+        return "DeviceParameterMessage{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                ", deviceCode='" + deviceCode + '\'' +
+                ", time=" + time +
+                '}';
+    }
+}
---
+++ acquisition-api/src/main/java/com/jinelei/numbfish/acquisition/influx/bean/DeviceStateMessage.java
@@ -0,0 +1,114 @@
+package com.jinelei.numbfish.acquisition.influx.bean;
+
+import com.jinelei.numbfish.acquisition.enumeration.RunningState;
+import com.jinelei.numbfish.common.helper.EnumerationHelper;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * @Author: jinelei
+ * @Description:
+ * @Date: 2024/7/14
+ * @Version: 1.0.0
+ */
+@SuppressWarnings("unused")
+public class DeviceStateMessage extends AbstractMessage {
+    public static final String DURATION = "duration";
+    public static final String STATUS = "status";
+    private Long duration;
+    private RunningState state;
+
+    @Override
+    public String bucket() {
+        return "DeviceState";
+    }
+
+    @Override
+    public String measurement() {
+        return getDeviceCode();
+    }
+
+    @Override
+    public Map<String, String> tags() {
+        return Map.of(DEVICE_CODE, getDeviceCode());
+    }
+
+    @Override
+    public Map<String, Object> fields() {
+        return Map.of(DURATION, getDuration(), STATUS, getState().ordinal());
+    }
+
+    @SafeVarargs
+    public final DeviceStateMessage parse(final Map<String, Object>... maps) {
+        for (Map<String, Object> map : maps) {
+            Optional.ofNullable(map.get(DEVICE_CODE)).map(Object::toString).ifPresent(this::setDeviceCode);
+            Optional.ofNullable(map.get(TIME)).map(Object::toString).map(Instant::parse).ifPresent(this::setTime);
+            Optional.ofNullable(map.get(FIELD)).map(Object::toString).ifPresent(it -> {
+                switch (it) {
+                    case STATUS -> Optional.ofNullable(map.get(VALUE)).map(Object::toString).map(Long::parseLong)
+                            .map(i -> EnumerationHelper.parseFrom(RunningState.class, i)).ifPresent(this::setState);
+                    case DURATION -> Optional.ofNullable(map.get(VALUE)).map(Object::toString).map(Long::parseLong)
+                            .ifPresent(this::setDuration);
+                    default -> {
+                    }
+                }
+            });
+        }
+        return this;
+    }
+
+    public DeviceStateMessage() {
+    }
+
+    public DeviceStateMessage(String deviceCode, Instant time, Long duration, RunningState state) {
+        super.setDeviceCode(deviceCode);
+        super.setTime(time);
+        this.duration = duration;
+        this.state = state;
+    }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+
+    public RunningState getState() {
+        return state;
+    }
+
+    public void setState(RunningState state) {
+        this.state = state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof DeviceStateMessage that))
+            return false;
+        if (!super.equals(o))
+            return false;
+        return Objects.equals(duration, that.duration) && state == that.state;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), duration, state);
+    }
+
+    @Override
+    public String toString() {
+        return "DeviceStateMessage{" +
+                "duration=" + duration +
+                ", state=" + state +
+                ", deviceCode='" + deviceCode + '\'' +
+                ", time=" + time +
+                '}';
+    }
+}
