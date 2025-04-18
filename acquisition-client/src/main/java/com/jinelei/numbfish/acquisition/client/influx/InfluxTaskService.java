49ff7f6f55bea72f64df41a6f1a562d3de71d879
jinelei <jinelei@163.com>
2025-04-18 23:17:45 +0800

    删除InfluxService及相关消息类，移除设备状态、参数和生产数据的处理逻辑

--- acquisition-client/src/main/java/com/jinelei/numbfish/acquisition/client/influx/InfluxTaskService.java
+++ acquisition-client/src/main/java/com/jinelei/numbfish/acquisition/client/influx/InfluxTaskService.java
@@ -1,5 +1,6 @@
 package com.jinelei.numbfish.acquisition.client.influx;
 
+import com.jinelei.numbfish.acquisition.influx.InfluxService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
