ab2a19738ef4774213f35629b7976dfe4aee38c5
jinelei <jinelei@163.com>
2025-04-19 18:04:13 +0800

    引入SpringHelper

--- acquisition-client/src/main/java/com/jinelei/numbfish/acquisition/NumbfishAcquisitionClientApplication.java
+++ acquisition-client/src/main/java/com/jinelei/numbfish/acquisition/NumbfishAcquisitionClientApplication.java
@@ -1,20 +1,24 @@
 package com.jinelei.numbfish.acquisition;
 
 import com.jinelei.numbfish.auth.configuration.annotation.EnableFeignAuthorization;
+import com.jinelei.numbfish.common.helper.SpringHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-@SpringBootApplication(scanBasePackageClasses = {NumbfishAcquisitionClientApplication.class})
+@SuppressWarnings("unused")
 @EnableFeignAuthorization
 @EnableFeignClients(basePackages = "com.jinelei.numbfish")
+@Import(SpringHelper.class)
+@SpringBootApplication(scanBasePackageClasses = {NumbfishAcquisitionClientApplication.class})
 public class NumbfishAcquisitionClientApplication {
     private static final Logger log = LoggerFactory.getLogger(NumbfishAcquisitionClientApplication.class);
 
